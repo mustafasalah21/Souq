552c26e739447c127970267bbe805fb60031740e
Mustafa_Salah <101256406+mustafasalah21@users.noreply.github.com>
2026-01-06 13:30:16 +0200

    Remove commented code in CategoriesController
    Removed commented-out code regarding overposting protection.

--- Souq/Controllers/CategoriesController.cs
+++ Souq/Controllers/CategoriesController.cs
@@ -48,9 +48,7 @@ namespace Souq.Controllers
             return View();
         }
 
-        // POST: Categories/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Descrption,Photo")] Category category)
